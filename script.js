4cdd5b0d9c51eee2f6aba15d910401b4f6b6fd55
Vesna Vele <divavesticakuga@gmail.com>
2018-04-05 18:42:36 +0200

    Sad ce sama Megi da se krece!

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var megi = document.querySelector('#megi');
+    megi.onload = function() {
+        megi.style.transition = 'left 20s linear 1s';
+        // megi.style.left = '1200px';
+        megi.style.left = '100%';
+
+    }
+})
